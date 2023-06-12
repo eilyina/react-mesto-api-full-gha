42186cd6d4df4673b6df0d8e7d9bdd5563cea470
eilyina <novokaterinka@gmail.com>
2023-06-12 21:36:36 +0500

    удалила corsOptions

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const corsOptions = {
-  origin: ['http://localhost:3000',
-  'http://eilyina.nomoredomains.rocks',
-  'https://eilyina.nomoredomains.rocks',
-  'http://api.eilyina.nomoredomains.rocks',
-  'https://api.eilyina.nomoredomains.rocks'],
+// const corsOptions = {
+//   origin: ['http://localhost:3000',
+//   'http://eilyina.nomoredomains.rocks',
+//   'https://eilyina.nomoredomains.rocks',
+//   'http://api.eilyina.nomoredomains.rocks',
+//   'https://api.eilyina.nomoredomains.rocks'],
 
-  methods: "GET,PUT,PATCH,POST,DELETE",
-}
+//   methods: "GET,PUT,PATCH,POST,DELETE",
+// }
 
 const app = express(); // создаём сервер
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 // подключаемся к серверу mongo
 app.use(express.json());
 // подключаемся к серверу mongo
