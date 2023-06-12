fe6f82c1a21a88a27c3605333baa59d8d7fcd287
eilyina <novokaterinka@gmail.com>
2023-06-12 19:21:43 +0500

    добавила cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express(); // создаём сервер
+app.use(cors);
 // подключаемся к серверу mongo
 app.use(express.json());
+// подключаемся к серверу mongo
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
